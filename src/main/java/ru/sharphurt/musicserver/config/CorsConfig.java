168f97eba7d5d2d3a59d94c11d05886c420dfa71
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2026-06-11 15:04:24 +0500

    Конфигурация CORS для localhost:3000

--- src/main/java/ru/sharphurt/musicserver/config/CorsConfig.java
+++ src/main/java/ru/sharphurt/musicserver/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://localhost:5173")
+                    .allowedOrigins("https://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
